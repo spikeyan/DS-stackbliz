0b91055af992795de64ef5374c1c852400590b90
spikeyan <spike_yan@163.com>
2022-03-13 15:57:48 +0800

    1 file modified

--- binaryTree/DroppingBalls.js
+++ binaryTree/DroppingBalls.js
@@ -11,16 +11,43 @@ let inputs = [
 ];
 let expect = [12, 7, 512, 3, 255, 36358];
 
+class Node {
+  id = '';
+  left = null;
+  right = null;
+  constructor(id) {
+    this.id = id;
+  }
+}
+
 function totalCounts(depth) {
   let sum = 0;
-  for (let i = 0; i < depth - 1; i++) {
-    sum += Math.pow(w, i);
+  for (let i = 0; i < depth; i++) {
+    sum += Math.pow(2, i);
   }
   return sum;
 }
 
-function DroppingBalls() {
-  console.log(12345);
+function createBinaryTree(total) {
+  let root = new Node(1);
+  let nodes = [root];
+  let i = 2;
+  for (let item of nodes) {
+    nodes.push((item.left = new Node(i)));
+    i++;
+    if (i == total) return root;
+    nodes.push((item.right = new Node(i)));
+    i++;
+    if (i == total) return root;
+  }
+}
+
+function DroppingBalls(arr) {
+  let [depth, balls] = arr;
+  depth = 3;
+  balls = 2;
+  let total = totalCounts(depth);
+  console.log(createBinaryTree(total));
 }
 
 console.log(test(DroppingBalls, inputs, expect));
